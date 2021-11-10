8e8233eaf1d6b286a2350dcb26ebb366db71774b
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2021-11-10 15:20:49 +0700

    make dashboard amin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $ticket['belum'] = Ticket::where('status',0)->get();
+        $ticket['sudah'] = Ticket::where('status',1)->get();
+        $ticket['batal'] = Ticket::where('status',3)->get();
+        $ticket['semua'] = Ticket::all();
+        return view('admin.index',compact('ticket'));
     }
 
     public function dashboarduser()
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,6 +17,82 @@
         </div>
         <div class="page-inner mt--5">
             <div class="row mt--2">
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-warning card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-users"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Belum Diatasi</p>
+                                        <h4 class="card-title">{{ count($ticket['belum']) }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-success card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-users"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Sudah Diatasi</p>
+                                        <h4 class="card-title">{{ count($ticket['sudah']) }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-danger card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-users"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Dibatalkan</p>
+                                        <h4 class="card-title">{{ count($ticket['batal']) }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-primary card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-users"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Semua Ticket</p>
+                                        <h4 class="card-title">{{ count($ticket['semua']) }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="col">
                     <div class="card full-height">
                         <div class="card-body">
@@ -48,7 +124,7 @@
         id:'circles-1',
         radius:45,
         value:60,
-        maxValue:100,
+        maxValue:1000,
         width:7,
         text: 5,
         colors:['#f1f1f1', '#FF9E27'],
